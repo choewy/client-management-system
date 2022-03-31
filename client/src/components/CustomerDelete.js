63757720b2904dd5aa88b7aa4bdcffed4e2a9a4e
choewy <choewy32@gmail.com>
2022-03-31 19:58:05 +0900

    고객 정보 삭제 시 다이얼로그 확인

--- client/src/components/CustomerDelete.js
+++ client/src/components/CustomerDelete.js
@@ -1,3 +1,10 @@
+import { useState } from "react";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
 import withStyles from "@mui/styles/withStyles";
 import { deleteCustomer } from "../actions/actions.customers";
 
@@ -5,6 +12,15 @@ const styles = (theme) => ({});
 
 const CustomerDelete = (props) => {
     const { customer_id, customers, setCustomers } = props;
+    const [open, setOpen] = useState(false);
+
+    const dialogOpenHandler = () => {
+        setOpen(true);
+    };
+
+    const dialogCloseHandler = () => {
+        setOpen(false);
+    };
 
     const customerDeleteHandler = async () => {
         const { success, message } = await deleteCustomer(customer_id);
@@ -12,9 +28,23 @@ const CustomerDelete = (props) => {
         setCustomers(customers.filter(customer => customer.customer_id !== customer_id));
     };
 
-    return (
-        <button onClick={customerDeleteHandler}>삭제</button>
-    );
+    return (<>
+        <Button variant="contained" color="error" onClick={dialogOpenHandler}>삭제</Button>
+        <Dialog open={open} onClose={dialogCloseHandler}>
+            <DialogTitle>
+                고객 정보를 삭제하시겠습니까?
+            </DialogTitle>
+            <DialogContent>
+                <Typography>
+                    해당 고객 정보가 삭제됩니다.
+                </Typography>
+            </DialogContent>
+            <DialogActions>
+                <Button variant="contained" color="primary" onClick={customerDeleteHandler}>삭제</Button>
+                <Button variant="outlined" color="primary" onClick={dialogCloseHandler}>취소</Button>
+            </DialogActions>
+        </Dialog>
+    </>);
 };
 
 export default withStyles(styles)(CustomerDelete);
