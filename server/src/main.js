b0c20c5a3e5dd33aca8b4b6cb8c2f58dd230632c
choewy <choewy32@gmail.com>
2022-03-31 14:06:58 +0900

    CORS 의존성 설치 및 설정

--- server/src/main.js
+++ server/src/main.js
@@ -1,9 +1,48 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true
+}));
 
 const port = process.env.PORT || 5000;
+
+app.get('/api/customers', (req, res) => {
+    console.log(1);
+    return res.json({
+        success: true,
+        rows: [
+            {
+                id: 1,
+                image: "https://placeimg.com/64/64/any",
+                name: "최원영",
+                birthday: "950302",
+                gender: "남자",
+                job: "선원"
+            },
+            {
+                id: 2,
+                image: "https://placeimg.com/64/64/any",
+                name: "테스트",
+                birthday: "950302",
+                gender: "남자",
+                job: "선원"
+            },
+            {
+                id: 3,
+                image: "https://placeimg.com/64/64/any",
+                name: "홍길동",
+                birthday: "950302",
+                gender: "남자",
+                job: "선원"
+            }
+        ]
+    });
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
